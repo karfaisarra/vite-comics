f91a97b2ec27cec2f864d1565a4b4a7e13e2adf6
karfaisarra <karfaisarra@gmail.com>
2022-11-10 15:31:17 +0100

    add header

---
+++ Vite-DC-Comics/src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import SiteHeader from './components/SiteHeader.vue'
+export default {
+  name: 'Vite-DC-Comics',
+  components: {
+    SiteHeader,
+  }
+}
+</script>
+<template>
+  <SiteHeader></SiteHeader>
+  <!-- /#site_header -->
+  <main id="site_main"></main>
+  <!-- /#site_main -->
+  <footer id="site_footer"></footer>
+  <!-- /#site_footer -->
+</template>
+<style>
+
+</style>
---
+++ Vite-DC-Comics/src/components/SiteHeader.vue
@@ -0,0 +1,89 @@
+<script>
+export default {
+    name: 'SiteHeader',
+    data() {
+        return {
+            menuEl: [
+                {
+                    name: 'CHARACTERS',
+                    href: '#'
+                },
+                {
+                    name: 'COMICS',
+                    href: '#'
+                },
+                {
+                    name: 'MOVIES',
+                    href: '#'
+                },
+                {
+                    name: 'TV',
+                    href: '#'
+                },
+                {
+                    name: 'GAMES',
+                    href: '#'
+                },
+                {
+                    name: 'COLLECTIBLES',
+                    href: '#'
+                },
+                {
+                    name: 'VIDEOS',
+                    href: '#'
+                },
+                {
+                    name: 'FANS',
+                    href: '#'
+                },
+                {
+                    name: 'NEWS',
+                    href: '#'
+                },
+                {
+                    name: 'SHOP',
+                    href: '#'
+                },
+            ]
+        }
+    }
+}
+</script>
+<template>
+    <header id="site_header">
+        <div class="navbar">
+            <div class="logo">
+                <img src="../assets/img/dc-logo.png" alt="">
+            </div>
+            <div class="menu">
+                <ul>
+                    <li v-for="el in menuEl">
+                        <a :href="el.href"> {{ el.name }}</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </header>
+</template>
+<style lang="scss" scoped>
+.navbar {
+    display: flex;
+    align-items: center;
+    padding: 1rem 2rem;
+    justify-content: space-around;
+
+    .menu ul {
+        list-style: none;
+        display: flex;
+
+        li {
+            padding: 0 1rem;
+
+            a {
+                text-decoration: none;
+                color: black;
+            }
+        }
+    }
+}
+</style>
