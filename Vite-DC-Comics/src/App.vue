3a9d3f3719066413030c67cc39922328a8852a97
karfaisarra <karfaisarra@gmail.com>
2022-11-10 16:12:30 +0100

    add section 1 and 2

--- Vite-DC-Comics/src/App.vue
+++ Vite-DC-Comics/src/App.vue
@@ -1,16 +1,18 @@
 <script>
-import SiteHeader from './components/SiteHeader.vue'
+import SiteHeader from './components/SiteHeader.vue';
+import SiteMaine from './components/SiteMain.vue';
 export default {
   name: 'Vite-DC-Comics',
   components: {
     SiteHeader,
+    SiteMaine,
   }
 }
 </script>
 <template>
   <SiteHeader></SiteHeader>
   <!-- /#site_header -->
-  <main id="site_main"></main>
+  <SiteMaine></SiteMaine>
   <!-- /#site_main -->
   <footer id="site_footer"></footer>
   <!-- /#site_footer -->
---
+++ Vite-DC-Comics/src/components/SiteMain.vue
@@ -0,0 +1,67 @@
+<script>
+export default {
+    name: 'SiteMain',
+    data() {
+        return {
+            icons: [
+                {
+                    image: '../assets/img/buy-comics-digital-comics.png',
+                    text: 'digital comics'
+                },
+                {
+                    image: '../assets/img/buy-comics-merchandise.png',
+                    text: 'dc merchandise'
+                },
+                {
+                    image: '../assets/img/buy-comics-subscriptions.png',
+                    text: 'subscription'
+                },
+                {
+                    image: '../assets/img/buy-comics-shop-locator.png',
+                    text: 'comic shop locator'
+                },
+                {
+                    image: '../assets/img/buy-dc-power-visa.svg',
+                    text: 'dc power visa'
+                },
+            ]
+        }
+    }
+}
+</script>
+<template>
+    <main id="site_main">
+        <section class="goes_here">
+            <div class="container">
+                <h2 class="text-white">--> Content goes here >-- </h2>
+            </div>
+        </section>
+        <section class="icons">
+            <div class="container">
+                <div class="row">
+                    <div class="col d-flex align-items-center" v-for="icon in icons">
+                        <div class="logo">
+                            <img :src="icon.image" alt="">
+                        </div>
+                        <div class="description ps-3">
+                            <h6 class="text-uppercase text-white">{{ icon.text }}</h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+</template>
+
+<style lang="scss" scoped>
+.goes_here {
+    padding: 2rem 1rem;
+    background-color: #1C1C1C;
+}
+
+.icons {
+    padding: 3rem 0;
+    background-color: #0282F9;
+
+}
+</style>
