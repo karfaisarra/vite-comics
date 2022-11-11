85054dd0bea737d45f45bbb9e534a94895da3766
karfaisarra <karfaisarra@gmail.com>
2022-11-11 15:41:17 +0100

    add comics array

---
+++ src/components/Comics.vue
@@ -0,0 +1,60 @@
+<script>
+import comics from '../components/data/comics.js'
+export default {
+    name: 'Comics',
+    data() {
+        return {
+            comics: comics,
+        }
+    }
+}
+</script>
+<template>
+    <section class="">
+        <div class="goes_here">
+            <div class="top container">
+                <a href="#">current series</a>
+            </div>
+        </div>
+        <div class="bottom">
+            <div class="container">
+                <div class="row row-cols-6">
+                    <div class="col" v-for="comic in comics">
+                        <img :src="comic.thumb" alt="">
+                        <h4>{{ comic.series }}</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+<style lang="scss" scoped>
+.goes_here {
+    height: 400px;
+    background-image: url(../assets/img/jumbotron.jpg);
+    background-repeat: no-repeat;
+
+    .top {
+        height: 100%;
+        position: relative;
+
+        a {
+            text-decoration: none;
+            color: white;
+            background-color: #0282F9;
+            padding: 0.5rem 1rem;
+            text-transform: uppercase;
+            font-weight: 600;
+            align-items: flex-end;
+            position: absolute;
+            bottom: -15px;
+            left: 0;
+        }
+    }
+}
+
+.bottom {
+    height: 400px;
+    background-color: #1C1C1C;
+}
+</style>
--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -1,74 +1,23 @@
 <script>
+import Comics from './Comics.vue'
+import Banner from './Banner.vue'
 export default {
-    name: 'SiteMain'
+    name: 'SiteMain',
+    components: {
+        Banner,
+        Comics
+    }
+
 }
 </script>
 <template>
     <main id="site_main">
-        <section class="goes_here">
-            <div class="container">
-                <h2 class="text-white">--> Content goes here >-- </h2>
-            </div>
-        </section>
-        <section class="icons">
-            <div class="container">
-                <div class="row">
-                    <div class="col d-flex align-items-center">
-                        <div class="logo">
-                            <img src="../assets/img/buy-comics-digital-comics.png" alt="">
-                        </div>
-                        <div class="description ps-3">
-                            <h6 class="text-uppercase text-white">digital comics</h6>
-                        </div>
-                    </div>
-                    <div class="col d-flex align-items-center">
-                        <div class="logo">
-                            <img src="../assets/img/buy-comics-merchandise.png" alt="">
-                        </div>
-                        <div class="description ps-3">
-                            <h6 class="text-uppercase text-white">dc merchandise</h6>
-                        </div>
-                    </div>
-                    <div class="col d-flex align-items-center">
-                        <div class="logo">
-                            <img src="../assets/img/buy-comics-subscriptions.png" alt="">
-                        </div>
-                        <div class="description ps-3">
-                            <h6 class="text-uppercase text-white">subscription</h6>
-                        </div>
-                    </div>
-                    <div class="col d-flex align-items-center">
-                        <div class="logo">
-                            <img src="../assets/img/buy-comics-shop-locator.png" alt="">
-                        </div>
-                        <div class="description ps-3">
-                            <h6 class="text-uppercase text-white">comic shop locator</h6>
-                        </div>
-                    </div>
-                    <div class="col d-flex align-items-center">
-                        <div class="logo">
-                            <img src="../assets/img/buy-dc-power-visa.svg" alt="">
-                        </div>
-                        <div class="description ps-3">
-                            <h6 class="text-uppercase text-white">dc power visa</h6>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
+        <Comics />
+        <Banner />
 
     </main>
 </template>
 
 <style lang="scss" scoped>
-.goes_here {
-    padding: 2rem 1rem;
-    background-color: #1C1C1C;
-}
 
-.icons {
-    padding: 3rem 0;
-    background-color: #0282F9;
-
-}
 </style>
